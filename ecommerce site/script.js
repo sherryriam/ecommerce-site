69c8f32fa0b0d6ab2266b2e2fd79bfe3663ecbe3
Mirriam <mirriamgacheri@gmail.com>
2024-07-19 12:40:29 +0300

    Add files via upload

---
+++ ecommerce site/script.js
@@ -0,0 +1,6 @@
+document.getElementById("nostock").innerHTML = "OPPS! WE RUN OUT OF STOCK";
+let name = "Miriam";
+let school = "MKU";
+let age = "30";
+
+document.getElementById("ownerinfo").innerHTML = `${name}, ${school},  ${age}.`;
